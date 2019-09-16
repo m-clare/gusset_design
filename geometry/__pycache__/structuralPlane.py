7209a80a91b1e3fb8c4b66a147adfd3e3a5805e6
Maryanne Wachter <mclare@utsv.net>
2019-09-16 18:52:35 -0400

    added plane

---
+++ geometry/__pycache__/structuralPlane.py
@@ -0,0 +1,12 @@
+__author__ = ['Maryanne Wachter', ]
+__license__ = 'Apache License, Version 2.0'
+__version__ = '0.1'
+__email__ = 'mclare@utsv.net'
+__status__ = 'Development'
+__date__ = 'Sept 16, 2019'
+
+
+class StructuralPlane(object):
+
+    def __init__(self):
+        pass
--- geometry/structuralNode.py
+++ geometry/structuralNode.py
@@ -8,10 +8,23 @@ __date__ = 'Sept 16, 2019'
 
 class StructuralNode(object):
 
-    def __init__(self, x=0.0, y=0.0, z=0.0):
+    def __init__(self, x=0.0, y=0.0, z=0.0, planes={}):
         self.x = x
         self.y = y
         self.z = z
+        self.planes = planes
+
+    def get_e_c(column_depth):
+    	return 0.5 * column_depth
+    	pass
+
+    def get_e_b(beam_depth):
+    	pass
+
+    def get_horizontal_gusset_force():
+    	pass
+
+    def
 
 
 if __name__ == "__main__":
