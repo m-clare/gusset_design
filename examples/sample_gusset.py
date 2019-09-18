bc291af2f387d82f1dcea60ab718a9e56bf5fec4
Maryanne Wachter <wachter@arch.ethz.ch>
2019-09-17 22:49:14 -0400

    fixed class method reference issue

--- examples/sample_gusset.py
+++ examples/sample_gusset.py
@@ -2,4 +2,8 @@ from gusset_design.reference.AISC_shapes_database import AISCShapesDatabase
 from gusset_design.members.structural_member import StructuralMember
 
 aisc_db = AISCShapesDatabase.from_json('../reference/aisc_shapes_database_v15.json')
-beam = StructuralMember.from_AISC_database()
+beam = StructuralMember.from_AISC_database('W16X26', aisc_db)
+column = StructuralMember.from_AISC_database('W14X145', aisc_db)
+brace = StructuralMember.from_AISC_database('HSS10X10X5/16', aisc_db)
+
+print(brace.__dict__)
