dbcbcdc5b8f23bf60052abbda935f63c8109b845
Maryanne Wachter <wachter@arch.ethz.ch>
2019-09-18 14:16:07 -0400

    working force interface methods added

--- examples/sample_gusset.py
+++ examples/sample_gusset.py
@@ -3,7 +3,12 @@ from gusset_design.members.structural_member import StructuralMember
 from gusset_design.geometry.gusset_plate import GussetPlate
 
 aisc_db = AISCShapesDatabase.from_json('../reference/aisc_shapes_database_v15.json')
-beam = StructuralMember.from_AISC_database('W16X26', aisc_db)
-column = StructuralMember.from_AISC_database('W14X145', aisc_db)
+beam = StructuralMember.from_AISC_database('W24X94', aisc_db)
+column = StructuralMember.from_AISC_database('W14X257', aisc_db)
 brace = StructuralMember.from_AISC_database('HSS10X10X5/16', aisc_db)
-gusset = GussetPlate('i', 36., 36., 125, brace, column, 'strong axis', beam)
+gusset = GussetPlate('i', 24.5, 28., 42.3, brace, column, 'strong axis', beam)
+
+print(gusset.__dict__)
+print(gusset.r)
+force = gusset.calculate_interface_forces(861, as_dict=True)
+print(force)
--- geometry/gusset_plate.py
+++ geometry/gusset_plate.py
@@ -21,34 +21,40 @@ class GussetPlate(object):
         self._beam = beam
         self._width = width
         self._height = height
-        self._eb = None
-        self._ec = None
-        self._beta_bar = None
-        self._alpha_bar = None
-        self._K_prime = None
-        self._K = None
-        self._D = None
-        self._beta = None
-        self._alpha = None
-        self._r = None
+
+    # Properties
 
     @property
     def brace_angle(self):
-        '''
-        Adjust brace angle for gusset plates in quadrant iii and iv
-        '''
-        if self._brace_angle > 180 or self._brace_angle < 0:
-            raise ValueError
-        if self._brace_angle > 90:
-            self._brace_angle = 180 - self._brace_angle
         return self._brace_angle
 
     @brace_angle.setter
-    def brace_angle(self, value):
+    def set_brace_angle(self, value):
         self._brace_angle = value
 
     @property
-    def eb(self):  # NEED TO ADD IN OPTION FOR SETTING eb in definition
+    def column_orientation(self):
+        return self._column_orientation
+
+    @column_orientation.setter
+    def set_column_orientation(self, value):
+        self._column_orientation = value
+
+    @property
+    def design_angle(self):
+        '''
+        Adjust brace angle for gusset plates in quadrant iii and iv
+        '''
+        if self.brace_angle > 180 or self.brace_angle < 0:
+            raise ValueError
+        if self.brace_angle > 90:
+            self._design_angle = 180 - self.brace_angle
+        else:
+            self._design_angle = self.brace_angle
+        return self._design_angle
+
+    @property
+    def eb(self):  # NEED TO ADD designTION FOR SETTING eb in definition
         if self._beam.Type == 'HSS':
             self._eb = self._beam.Ht * 0.5
         else:
@@ -57,9 +63,9 @@ class GussetPlate(object):
 
     @property
     def ec(self):
-        if self._column_orientation == 'strong axis':
+        if self.column_orientation == 'strong axis':
             self._ec = self._column.d * 0.5
-        if self._column_orientation == 'weak axis':
+        if self.column_orientation == 'weak axis':
             self._ec = self._column.bf * 0.5
         return self._ec
 
@@ -75,30 +81,30 @@ class GussetPlate(object):
 
     @property
     def K_prime(self):
-        self._K_prime = self.alpha_bar * tan(radians(self.brace_angle)) + \
-                       self.alpha_bar / self.beta_bar
+        self._K_prime = self.alpha_bar * (tan(radians(self.design_angle)) +
+                        self.alpha_bar / self.beta_bar)
         return self._K_prime
 
     @property
     def D(self):
-        self._D = tan(radians(self.brace_angle)) ** 2. + \
+        self._D = tan(radians(self.design_angle)) ** 2. + \
                  (self.alpha_bar / self.beta_bar) ** 2.
         return self._D
 
     @property
     def K(self):
-        self._K = self.eb * tan(radians(self.brace_angle)) - self.ec
+        self._K = self.eb * tan(radians(self.design_angle)) - self.ec
         return self._K
 
     @property
     def beta(self):
-        self._beta = self.K_prime - \
-                    self.K * tan(radians(self.brace_angle)) / self.D
+        self._beta = (self.K_prime -
+                      self.K * tan(radians(self.design_angle))) / self.D
         return self._beta
 
     @property
     def alpha(self):
-        self._alpha = (self.K_prime * tan(radians(self.brace_angle)) +
+        self._alpha = (self.K_prime * tan(radians(self.design_angle)) +
                        self.K * (self.alpha_bar / self.beta_bar) ** 2.) \
                       / self.D
         return self._alpha
@@ -107,6 +113,32 @@ class GussetPlate(object):
     def r(self):
         self._r = ((self.alpha + self.ec) ** 2. +
                    (self.beta + self.eb) ** 2.) ** 0.5
+        return self._r
+
+    # Methods
+    def calculate_column_interface_forces(self, brace_force, as_dict=False):
+        V_c = self.beta * brace_force / self.r
+        H_c = self.ec * brace_force / self.r
+        M_c = H_c * (self.beta - self.beta_bar)
+        if as_dict:
+            return {'V_c': V_c, 'H_c': H_c, 'M_c': M_c}
+        return V_c, H_c, M_c
+
+    def calculate_beam_interface_forces(self, brace_force, as_dict=False):
+        V_b = self.eb * brace_force / self.r
+        H_b = self.alpha * brace_force / self.r
+        M_b = V_b * (self.alpha - self.alpha_bar)
+        if as_dict:
+            return {'V_b': V_b, 'H_b': H_b, 'M_b': M_b}
+        return V_b, H_b, M_b
+
+    def calculate_interface_forces(self, brace_force, as_dict=False):
+        V_c, H_c, M_c = self.calculate_column_interface_forces(brace_force)
+        V_b, H_b, M_b = self.calculate_beam_interface_forces(brace_force)
+        if as_dict:
+            return {'V_c': V_c, 'H_c': H_c, 'M_c': M_c,
+                    'V_b': V_b, 'H_b': H_b, 'M_b': M_b}
+        return V_c, H_c, M_c, V_b, H_b, M_b
 
     # utilize geometric limits
 
